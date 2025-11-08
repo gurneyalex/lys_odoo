c25c58cc93527d56e6899e2ffcd3294ef7e90530
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2025-11-08 11:07:17 +0100

    bootstrap project, first modules

---
+++ src/odoo/addons/lys_history/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Lys: Historic Data",
+    "license": "AGPL-3",
+    "author": "Alexandre Fayolle <alexandre.fayolle@free.fr>",
+    "category": "History",
+    "data": ["data/res_currency.xml",
+    ],
+    "depends": ["base"],
+}
+
