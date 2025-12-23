3b192f5d6bbd03baebd828355018f44497cc4a02
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2025-12-23 16:06:31 +0100

    [IMP] clothing caracteristics model and view

--- src/odoo/addons/lys_history/models/history_clothing_caracteristic.py
+++ src/odoo/addons/lys_history/models/history_clothing_caracteristic.py
@@ -3,6 +3,6 @@ from odoo import models, fields
 
 class HistoryClothingCaracteristic(models.Model):
     _name = "history.clothing.caracteristic"
-    _description = "Clothing Caracteristic"
+    _description = "Clothing Caracteristic, used as tags on a clothing purchase"
 
     name = fields.Char(required=True)
