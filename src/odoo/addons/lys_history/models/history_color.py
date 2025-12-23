6b280b19c44974abb54b9c1e629bda393cd50e80
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2025-12-23 18:11:00 +0100

    [IMP] history.color
    * rename color annotation -> subcategory
    * add display_name

--- src/odoo/addons/lys_history/models/history_color.py
+++ src/odoo/addons/lys_history/models/history_color.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import api, fields, models
 
 
 class HistoryColor(models.Model):
@@ -6,4 +6,13 @@ class HistoryColor(models.Model):
     _description = "Color"
 
     name = fields.Char(required=True)
-    annotation = fields.Char()
+    sub_category = fields.Char()
+
+    @api.depends("name", "sub_category")
+    def _compute_display_name(self):
+        for record in self:
+            if record.sub_category:
+                display_name = f"{record.name} ({record.sub_category})"
+            else:
+                display_name = record.name
+            record.display_name = display_name
